d028020d3b9bbad9111efdba5aaf7b3997947f1f
salimsoltani28 <salimsoltani28@gmail.com>
2023-07-09 15:01:49 +0200

    adding first scripts of data journalism training

---
+++ Data journalism_tutorial_1_data_type_DataStructure.R
@@ -0,0 +1,46 @@
+
+
+#data type : Explanation of each type: Numeric, Integer, Logical, and Character.
+# Numeric (decimal)
+num_var <- 12.5
+print(class(num_var))
+
+# Integer
+int_var <- 2L
+print(class(int_var))
+
+
+# Logical
+log_var <- TRUE
+print(class(log_var))
+
+# Character (string)
+char_var <- "Hello, R!"
+print(class(char_var))
+
+
+##### Explanation of each structure: Vector, Matrix, List, Data Frame, and Factor.
+
+# Vector
+vec <- c(1,2,3)
+print(class(vec))
+
+# Matrix
+mat <- matrix(c(1,2,3,4), nrow = 2)
+print(class(mat))
+
+# List
+lst <- list("Red", "Green", "Blue")
+print(class(lst))
+
+# Data Frame
+df <- data.frame(name=c("John", "Jane"), age=c(23, 24))
+print(class(df))
+
+# Factor
+fac <- factor(c("male", "female", "male"))
+print(class(fac))
+
+
+#### Project: create your own data and implement what you have learned in the above
+#1- Idea 1: create an online excel sheet and fill out your information. For example name , work experinece etc.
