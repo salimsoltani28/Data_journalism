1cacca65183b9e1403807d77cdc5a8f3115e9f3f
salimsoltani28 <salimsoltani28@gmail.com>
2023-07-11 00:42:22 +0200

    adding more update

--- Data journalism_tutorial_1_data_type_DataStructure.R
+++ Data journalism_tutorial_1_data_type_DataStructure.R
@@ -44,3 +44,16 @@ print(class(fac))
 
 #### Project: create your own data and implement what you have learned in the above
 #1- Idea 1: create an online excel sheet and fill out your information. For example name , work experinece etc.
+
+#control flow: 
+temperatures <- c(13, 15, 22, 27, 14, 19, 24, 29, 12, 17, 21, 26)
+
+for (temperature in temperatures) {
+  if (temperature < 15) {
+    print("Cold")
+  } else if (temperature >= 15 & temperature <= 25) {
+    print("Moderate")
+  } else {
+    print("Hot")
+  }
+}
